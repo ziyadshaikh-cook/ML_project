6edd84746adc1aa39f8cd47fc310fa66b240afa5
Ziyad Shaikh <ziyadshaikh98@gmail.com>
2026-04-12 19:21:28 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from src.ML_project.logger import logging
 from src.ML_project.exception import CustomException
+from src.ML_project.components.data_ingestion import DataIngestion
+from src.ML_project.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__ == "__main__":
     logging.info("The execution has started")
 
     try:
-        a= 1/0
+        #data_ingestion_config=Dataingestionconfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
---
+++ src/ML_project/utils.py
@@ -0,0 +1,32 @@
+import os
+import sys 
+from src.ML_project.exception import CustomException
+from src.ML_project.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb=pymysql.connect(
+            host = host,
+            user = user,
+            password = password,
+            db = db
+        )
+        logging.info(f"Connection esatablished:{mydb}")
+        df = pd.read_sql_query("Select * from students",mydb)
+        print(df.head())
+
+        return df
+    
+    except Exception as ex:
+        raise CustomException(ex)
