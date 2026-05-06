0d6c8f4628262bd5f504f1b96195228405753aed
Ziyad Shaikh <ziyadshaikh98@gmail.com>
2026-05-07 00:07:54 +0530

    feat: add Flask web app with prediction form and result display

--- app.py
+++ app.py
@@ -1,29 +1,38 @@
-from src.ML_project.logger import logging
-from src.ML_project.exception import CustomException
-from src.ML_project.components.data_ingestion import DataIngestion
-from src.ML_project.components.data_ingestion import DataIngestionConfig
-from src.ML_project.components.data_transformation import DataTransformationConfig,DataTransformation
-from src.ML_project.components.model_trainer import ModelTrainerConfig,ModelTrainer
-import sys
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from src.ML_project.pipelines.prediction_pipelines import CustomData, PredictPipeline
+
+application = Flask(__name__)
+app = application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
+
+        pred_df = data.get_data_as_dataframe()
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+
+        return render_template('home.html', results=round(results[0], 2))
 
 if __name__ == "__main__":
-    logging.info("The execution has started")
-
-    try:
-        #data_ingestion_config=Dataingestionconfig()
-        data_ingestion = DataIngestion()
-        train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
-
-        #data_transformation_config = DataIngestionConfig()
-        data_transformation = DataTransformation()
-        train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)    
-
-        ##model trainner
-        model_trainer = ModelTrainer()
-        model_trainer.initiate_model_trainer(train_arr,test_arr)
-        
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
-    
-
+    app.run(host="0.0.0.0", debug=True)
